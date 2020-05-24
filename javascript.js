c7fbbf628e185ea66e3bd3162bff46e89c84f75f
raccoon <petrushko.ilya@gmail.com>
2020-05-24 22:00:01 +0300

    added Kostya's javascript

--- javascript.js
+++ javascript.js
@@ -1,22 +1,49 @@
 
 
 
-var theme = document.getElementsByClassName("classic-theme");
+// var theme = document.getElementsByClassName("classic-theme");
+// var themeButtone = document.getElementById("theme-buttone");
+
+//     themeButtone.onclick = function(){
+
+//         if(theme.className == "dark-theme"){
+
+//             theme.className = "classic-theme";
+//             themeButtone.innerHTML = "Dark Theme";
+            
+
+//         } else{
+
+//             theme.className = "dark-theme";
+//             themeButtone.innerHTML = "Classic Theme";
+
+//         }
+
+//     };
+
+var theme = document.getElementsByClassName("theme");
 var themeButtone = document.getElementById("theme-buttone");
 
     themeButtone.onclick = function(){
 
-        if(theme.className == "dark-theme"){
+        var className = Array.from(theme[0].classList);
 
-            theme.className = "classic-theme";
+        if (className.includes("dark")){
+            toggleTheme( "dark","classic")
+
             themeButtone.innerHTML = "Dark Theme";
-            
 
-        } else{
+        } else if (className.includes("classic")) {
+            toggleTheme("classic", "dark")
 
-            theme.className = "dark-theme";
             themeButtone.innerHTML = "Classic Theme";
-
         }
-
     };
+
+    function toggleTheme(removeTheme, addTheme) {
+        for (var i = 0; i < theme.length; i++) {
+
+            theme[i].classList.remove(removeTheme);
+            theme[i].classList.add(addTheme);
+        }
+    }
